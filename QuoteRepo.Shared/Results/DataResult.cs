e113bba148d99d1eb89e0d65dfc572c7da90b2b1
hanoguzakpinar <oguzhanakpinar@yahoo.com>
2022-08-23 10:33:25 +0300

    dataresult refactor

--- QuoteRepo.Shared/Results/DataResult.cs
+++ QuoteRepo.Shared/Results/DataResult.cs
@@ -4,41 +4,15 @@ namespace QuoteRepo.Shared.Results
 {
     public class DataResult<T> : IDataResult<T>
     {
-
-        public DataResult()
+        public DataResult(ResultStatus resultStatus, string message)
         {
-            Errors = new List<ValidationFailure>();
-        }
-        public DataResult(T data)
-        {
-            Data = data;
-        }
-
-        public DataResult(string? message)
-        {
+            ResultStatus = resultStatus;
             Message = message;
         }
 
         public DataResult(ResultStatus resultStatus, T data)
         {
             ResultStatus = resultStatus;
-            Data = data;
-        }
-        public DataResult(ResultStatus resultStatus, string? message)
-        {
-            ResultStatus = resultStatus;
-            Message = message;
-        }
-        public DataResult(ResultStatus resultStatus, string message, T data)
-        {
-            ResultStatus = resultStatus;
-            Data = data;
-            Message = message;
-        }
-        public DataResult(ResultStatus resultStatus, List<ValidationFailure> errors, T data)
-        {
-            ResultStatus = resultStatus;
-            Errors = errors;
             Data = data;
         }
 
