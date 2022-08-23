8cebbf4a1dd96d72931e51001f51567d2b4c3139
hanoguzakpinar <oguzhanakpinar@yahoo.com>
2022-08-23 11:20:35 +0300

    result refactorin'

--- QuoteRepo.Shared/Results/Result.cs
+++ QuoteRepo.Shared/Results/Result.cs
@@ -4,32 +4,16 @@ namespace QuoteRepo.Shared.Results
 {
     public class Result : IResult
     {
-        public Result()
-        {
-            Errors = new List<ValidationFailure>();
-        }
-        public Result(ResultStatus resultStatus)
-        {
-            ResultStatus = resultStatus;
-        }
         public Result(ResultStatus resultStatus, string message)
         {
             ResultStatus = resultStatus;
             Message = message;
         }
+
         public Result(ResultStatus resultStatus, List<ValidationFailure> errors)
         {
             ResultStatus = resultStatus;
             Errors = errors;
-        }
-        public Result(List<ValidationFailure> errors)
-        {
-            Errors = errors;
-        }
-
-        public Result(string message)
-        {
-            Message = message;
         }
 
         public ResultStatus ResultStatus { get; }
