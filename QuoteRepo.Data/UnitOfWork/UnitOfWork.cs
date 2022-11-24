923fd88539d8d585edb01eb67571c9a00231adc4
hanoguzakpinar <oguzhanakpinar@yahoo.com>
2022-11-24 15:36:06 +0300

    unitOfwork eklendi

---
+++ QuoteRepo.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using QuoteRepo.Core.UnitOfWorks;
+
+namespace QuoteRepo.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly QuoteContext _context;
+
+        public UnitOfWork(QuoteContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
